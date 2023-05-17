4b113f952bfdf2fba9fe8e4a59c41d47eb0d9fff
devsarvesh92 <sarveshdev92@gmail.com>
2023-05-17 09:26:18 +0530

    Code changes in target sum

--- target_sum.py
+++ target_sum.py
@@ -1,9 +1,11 @@
 def twoSum(nums: list[int], target: int) -> list[int]:
+    try:
+        for id, num in enumerate(nums):
+            diff = target - num
+            if pair := nums.index(diff):
+                return [id, pair]
+    except ValueError:
+        print("Ignore")
 
-    for id, num in enumerate(nums):
-        diff = target - num
-        if pair := nums.index(diff):
-            return [id, pair]
 
-
-print(twoSum(nums=[2, 7, 11, 15], target=9))
+print(twoSum(nums=[3, 3], target=6))
