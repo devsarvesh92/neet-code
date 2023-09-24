a0a8653c8515bc7b230ea163a992e100c498b814
devsarvesh92 <sarveshdev92@gmail.com>
2023-09-24 18:17:39 +0530

    Reverse Linked List

--- tests/test_reverse_link_list.py
+++ tests/test_reverse_link_list.py
@@ -1,6 +1,6 @@
 import pytest
 
-from linked_list.reverse_link_list import LinkedList
+from linked_list.ll_reversal_problem import LinkedList
 
 
 pytestmark = pytest.mark.reverselinklist
@@ -10,5 +10,5 @@ def test_reverse_linked_list():
     ll = LinkedList()
     for val in [1, 2, 3, 4, 5]:
         ll.insert_at_end(val=val)
-    ll.reverse()
+    ll.reverse_using_two_ptrs()
     assert str(ll) == "->5->4->3->2->1"
