9aac5ce9b6ca7fc5aff2607d7d1d85b13497c072
devsarvesh92 <sarveshdev92@gmail.com>
2024-01-12 23:21:18 +0530

    day-6 | jan-12 | Solve three sum

--- blind_75/two_ptrs/two_ptrs_problems.py
+++ blind_75/two_ptrs/two_ptrs_problems.py
@@ -14,3 +14,24 @@ def is_palindrome(s: str) -> bool:
         end -= 1
 
     return True
+
+
+def three_sum(nums: list[int]) -> list[list[int]]:
+    target_sum = 0
+    nums.sort()
+    result = []
+    for id, num in enumerate(nums):
+        i, j = id + 1, len(nums) - 1
+
+        while i < j:
+            current_sum = num + nums[i] + nums[j]
+            if target_sum == current_sum:
+                if [num, nums[i], nums[j]] not in result:
+                    result.append([num, nums[i], nums[j]])
+                i += 1
+                j -= 1
+            elif current_sum > target_sum:
+                j -= 1
+            elif current_sum < target_sum:
+                i += 1
+    return result
--- tests/test_two_ptrs_problems.py
+++ tests/test_two_ptrs_problems.py
@@ -1,5 +1,5 @@
 import pytest
-from blind_75.two_ptrs.two_ptrs_problems import is_palindrome
+from blind_75.two_ptrs.two_ptrs_problems import is_palindrome, three_sum
 
 pytestmark = pytest.mark.twoptrsproblems
 
@@ -7,3 +7,7 @@ pytestmark = pytest.mark.twoptrsproblems
 def test_valid_palindrome():
     # https://leetcode.com/problems/valid-palindrome/submissions/1144361720/
     assert True == is_palindrome(s="A man, a plan, a canal: Panama")
+
+
+def test_three_sum():
+    assert [[-1, -1, 2], [-1, 0, 1]] == three_sum(nums=[-1, 0, 1, 2, -1, -4])
