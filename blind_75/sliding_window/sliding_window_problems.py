5a09702f583b3f8cb85283b4f7aae17c7f0378d2
devsarvesh92 <sarveshdev92@gmail.com>
2024-01-17 11:31:40 +0530

    day-9 | jan-17 | Longest Substring Without Repeating Characters

--- blind_75/sliding_window/sliding_window_problems.py
+++ blind_75/sliding_window/sliding_window_problems.py
@@ -11,11 +11,25 @@ def max_profit(prices:list[int])->int:
             l=r
         r+=1
     return profit
+
+
+def longest_substring_without_rep_chars(input:str):
+    i,j = 0,0
+    window=set()
+    long_len=0
+    while j<len(input):
+        if input[j] not in window:
+            window.add(input[j])
+            long_len = max(len(window),long_len)
+            j+=1
+        else:
+            i+=1
+            j=i
+            window=set()
+    return long_len
+
         
 
-    
         
+         
 
-
-
-    
--- tests/test_sliding_window_problems.py
+++ tests/test_sliding_window_problems.py
@@ -1,6 +1,6 @@
 import pytest
 
-from blind_75.sliding_window.sliding_window_problems import max_profit
+from blind_75.sliding_window.sliding_window_problems import max_profit,longest_substring_without_rep_chars
 
 pytestmark = pytest.mark.slidingwindowproblems
 
@@ -8,3 +8,6 @@ pytestmark = pytest.mark.slidingwindowproblems
 def test_max_profit():
     # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/1147929471/
     assert 5 == max_profit(prices=[7,1,5,3,6,4])
+
+def test_longest_substring_without_rep_chars():
+    assert 2 == longest_substring_without_rep_chars(input="aab")
