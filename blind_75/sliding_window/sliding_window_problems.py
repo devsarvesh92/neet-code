96125f81ee75638504afb76becad2603766441f2
devsarvesh92 <sarveshdev92@gmail.com>
2024-01-17 22:13:52 +0530

    day-9 | jan-17 | Longest Repeating Character Replacement

--- blind_75/sliding_window/sliding_window_problems.py
+++ blind_75/sliding_window/sliding_window_problems.py
@@ -1,35 +1,50 @@
+import string
 
 
-def max_profit(prices:list[int])->int:
+def max_profit(prices: list[int]) -> int:
     profit = 0
-    l,r = 0,1
+    l, r = 0, 1
     while r < len(prices):
-        bp= prices[l]
+        bp = prices[l]
         sp = prices[r]
-        profit = max(profit,sp-bp)
+        profit = max(profit, sp - bp)
         if sp < bp:
-            l=r
-        r+=1
+            l = r
+        r += 1
     return profit
 
 
-def longest_substring_without_rep_chars(input:str):
-    i,j = 0,0
-    window=set()
-    long_len=0
-    while j<len(input):
+def longest_substring_without_rep_chars(input: str):
+    i, j = 0, 0
+    window = set()
+    long_len = 0
+    while j < len(input):
         if input[j] not in window:
             window.add(input[j])
-            long_len = max(len(window),long_len)
-            j+=1
+            long_len = max(len(window), long_len)
+            j += 1
         else:
-            i+=1
-            j=i
-            window=set()
+            i += 1
+            j = i
+            window = set()
     return long_len
 
-        
 
-        
-         
+def longest_rep_char_replacement(s: str, k: int) -> int:
+    counter = dict.fromkeys(string.ascii_uppercase, 0)
+    max_occurence = 0
+    i, j = 0, 0
+    max_len = 0
+    # AABABBA
 
+    while j < len(s):
+        counter[s[j]] += 1
+        max_occurence = max(counter[s[j]], max_occurence)
+        window_len = j - i + 1
+        if window_len - max_occurence <= k:
+            max_len = max(max_len, window_len)
+        else:
+            counter[s[i]] -= 1
+            i += 1
+        j += 1
+    return max_len
--- tests/test_sliding_window_problems.py
+++ tests/test_sliding_window_problems.py
@@ -1,13 +1,23 @@
 import pytest
 
-from blind_75.sliding_window.sliding_window_problems import max_profit,longest_substring_without_rep_chars
+from blind_75.sliding_window.sliding_window_problems import (
+    max_profit,
+    longest_substring_without_rep_chars,
+    longest_rep_char_replacement,
+)
 
 pytestmark = pytest.mark.slidingwindowproblems
 
 
 def test_max_profit():
     # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/1147929471/
-    assert 5 == max_profit(prices=[7,1,5,3,6,4])
+    assert 5 == max_profit(prices=[7, 1, 5, 3, 6, 4])
+
 
 def test_longest_substring_without_rep_chars():
     assert 2 == longest_substring_without_rep_chars(input="aab")
+
+
+def test_longest_rep_char_replacement():
+    # https://leetcode.com/problems/longest-repeating-character-replacement/submissions/1148968162/
+    assert 4 == longest_rep_char_replacement(s="AABABBA", k=1)
