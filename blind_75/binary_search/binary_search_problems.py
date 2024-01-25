7d8348c2c414c9fc02672647a5367226b183a931
devsarvesh92 <sarveshdev92@gmail.com>
2024-01-25 22:43:46 +0530

    day-12 | jan-25 | Binary search

--- blind_75/binary_search/binary_search_problems.py
+++ blind_75/binary_search/binary_search_problems.py
@@ -10,3 +10,18 @@ def search_element(nums: list[int], num: int) -> int:
         else:
             return mid
     return -1
+
+
+def find_min(nums: list[int]) -> int:
+    l, r = 0, len(nums) - 1
+    min_num = nums[0]
+    while l <= r:
+        mid = (l + r) // 2
+        min_num = min(min_num, nums[mid], nums[l], nums[r])
+        middle = nums[mid]
+        left = nums[l]
+        if middle >= left:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return min_num
--- tests/test_binary_search_problems.py
+++ tests/test_binary_search_problems.py
@@ -1,8 +1,13 @@
 import pytest
-from blind_75.binary_search.binary_search_problems import search_element
+from blind_75.binary_search.binary_search_problems import search_element, find_min
 
 pytestmark = pytest.mark.binarysearchproblems
 
 
 def test_binary_search():
     assert 7 == search_element(nums=[1, 2, 3, 4, 5, 6, 7, 8], num=8)
+
+
+def test_find_min():
+    # https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/submissions/1156667651/
+    assert 1 == find_min(nums=[3, 4, 5, 1, 2])
